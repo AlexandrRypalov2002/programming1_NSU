a4ca931ed413b2c0ab729e79f97efe95c2814006
AlexandrRypalov2002 <79108257+AlexandrRypalov2002@users.noreply.github.com>
2021-03-09 23:54:01 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,49 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define N 255
+
+void swap(int* a, int* b) {
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
+
 int main() {
-	int w, h; //h - столбцы, w - строчки
-	int M[N][N];
-	int a = 0;
-	int b = 0;
+	int w, h;
+	int A[N][N];
+	int B[N][N];
+	int C[N] = { 0 };
+	int D[N] = { 0 };
 	scanf("%d %d", &w, &h);
-	int A[N];
-	for (int j = 0; j < w; j++) {
-		A[j] = 0;
+	for (int i = 0; i < w; i++) {
+		D[i] = i;
 	}
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w; j++) {
-			scanf("%d", &M[i][j]);
+			scanf("%d", &A[i][j]);
 		}
 	}
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w; j++) {
-			if ((M[i][j] < 0) && ((M[i][j]) % 2 != 0)) {
-				A[j] += M[i][j];
+			if ((A[i][j] < 0) && (A[i][j] % 2 != 0)) {
+				C[j] += A[i][j];
 			}
 		}
 	}
-	//здесь начинается сортировка пузырьком массива из характеристик
-	for (int k = w - 1; k >= 0; k--) {
-		for (int j = 0; j < k; j++) {
-			if (A[j] > A[j + 1]) {
-				a = A[j];
-				A[j] = A[j + 1];
-				A[j + 1] = a;
-				for (int i = 0; i < h; i++) {
-					b = M[i][j];
-					M[i][j] = M[i][j+1];
-					M[i][j+1] = b;
-				}
-			}
+	for (int i = 1; i < w; i++) {
+		for (int j = i; j > 0 && C[j - 1] > C[j]; j--) {
+			swap(&C[j - 1], &C[j]);
+			swap(&D[j - 1], &D[j]);
 		}
 	}
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w; j++) {
-			printf("%d ", M[i][j]);
+			B[i][j] = A[i][D[j]];
+		}
+	}
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			printf("%d ", B[i][j]);
 		}
 		printf("\n");
 	}
-	printf("%d %d", A[0], A[w - 1]);
+	printf("%d %d", C[0], C[w - 1]);
 	return 0;
 }
